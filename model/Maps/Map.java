d22cdf9a2996a40e74695f46473c4d4a920153a8
J. David Taylor <35313849+jdavidtaylor496@users.noreply.github.com>
2018-03-27 23:29:39 -0700

    added path class and some comments

--- model/Maps/Map.java
+++ model/Maps/Map.java
@@ -2,7 +2,14 @@ package model.Maps;
 
 
 public abstract class Map {
-
+	
+	private Path mobRoute; // This should be enough data to tell monsters where to walk and the mapView what background graphics to use.
+	
+	// A map should contain data about where monsters can walk, where towers can be placed, 
+	// where the end zone is, where things spawn, what graphics to use (by filename), 
+	// what music to use (by filename), which towers and monsters to use, etc.
+	
+	
 	public Map ()
 	{
 		
---
+++ model/Maps/Path.java
@@ -0,0 +1,7 @@
+package model.Maps;
+
+// This class will encapsulate the data of a walkable path from start to 
+// finish. Each map will have a different path.
+public class Path {
+
+}
