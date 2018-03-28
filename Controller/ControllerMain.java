4669b83f3124a7c711aa48f6dfb98dce0a78025d
David Weinflash <davidw@email.arizona.edu>
2018-03-27 20:04:02 -0700

    Trying to add dependencies to ControllerMain

--- Controller/ControllerMain.java
+++ Controller/ControllerMain.java
@@ -1,4 +1,15 @@
 
 public class ControllerMain {
 
+	public Map theMap;
+	public Mob	theMob;
+	public Tower theTower;
+	
+	public ControllerMain()
+	{
+		theMap = new Map();
+		theMob = new Mob();
+		theTower = new Tower();
+	}
+	
 }
--- Model/Maps/Map.java
+++ Model/Maps/Map.java
@@ -1,4 +1,9 @@
 
 public abstract class Map {
 
+	public Map ()
+	{
+		
+	}
+	
 }
