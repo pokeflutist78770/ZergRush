b6af1265b8afb323557b2e40f37d1486c390f3dd
J. David Taylor <35313849+jdavidtaylor496@users.noreply.github.com>
2018-03-27 19:24:46 -0700

    reorganized

--- Model/Mobs/Mob.java
+++ Model/Mobs/Mob.java
@@ -2,7 +2,7 @@
 public abstract class Mob {
 	
 	private Speed speed;
-	private DefenseType defense;
+	private DefenseType defense; // i.e. hp
 	private ArmorType armor;
 	private List<Resistance> resistances;
 
