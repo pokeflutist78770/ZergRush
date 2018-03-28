918d868868b5b29d7373ce3aed28240927c8da39
J. David Taylor <35313849+jdavidtaylor496@users.noreply.github.com>
2018-03-27 23:20:14 -0700

    added projectile object

---
+++ model/Projectile.java
@@ -0,0 +1,7 @@
+package model;
+
+// This is the object that is instantiated when the tower shoots. 
+//It keeps track of its location and it moves along until it reaches its target.
+public class Projectile {
+
+}
