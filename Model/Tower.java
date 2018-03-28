62ef8f9068ee7633c6e544ccc86a3bc761b32d1d
J. David Taylor <35313849+jdavidtaylor496@users.noreply.github.com>
2018-03-27 19:22:18 -0700

    more stubs

--- Model/Tower.java
+++ Model/Tower.java
@@ -1,4 +1,10 @@
 
 public abstract class Tower {
+	
+	private int cost;
+	
+	private Range range;
+	private int numberOfAttacks;
+	private Ammunition ammmo; // This is meant to represent the type of thing a tower shoots. 
 
 }
