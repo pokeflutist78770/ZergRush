33096bd8b692cccf254560d1b6411a72c17cb727
David Weinflash <davidw@email.arizona.edu>
2018-04-07 15:28:18 -0700

    PLEASE GOD UPLOAD DDUBS STUFF

--- views/InstructionView.java
+++ views/InstructionView.java
@@ -1,5 +1,11 @@
 package views;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
+
 // You are required to have an instruction page. Treat this as a user-guide
 // to how to play your game. This guide should be found in the starting menu.
 
@@ -11,6 +17,21 @@ package views;
 
 // The player can read the User-Guide of the game anytime before game-play.
 
-public class InstructionView {
+public class InstructionView extends StackPane {
 
+	private Button backButton;
+	private Label header;
+	private BorderPane pane;
+	
+	public InstructionView(Button back)
+	{
+		backButton = back;
+		header = new Label("Instructions Page");
+		pane = new BorderPane();
+		
+		pane.setTop(header);
+		pane.setBottom(backButton);
+		this.getChildren().add(pane);
+	}
+	
 }
--- views/MenuView.java
+++ views/MenuView.java
@@ -1,9 +1,33 @@
 package views;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+
 // You should have a starting screen prior to the maps.
 
 // The players should have the choice of choosing between maps.
 
-public class MenuView {
+public class MenuView extends StackPane {
 
+	private Button startButton;
+	private Button instrButton;
+	private GridPane grid;
+	
+	public MenuView(Button start, Button instr)
+	{
+		grid = new GridPane();
+		startButton = start;
+		instrButton = instr;
+		
+		grid.add(startButton, 0, 0);
+		grid.add(instrButton, 0, 1);
+		this.getChildren().add(grid);
+		
+		//StackPane.setAlignment(grid, Pos.CENTER);
+		//this.setCenter(grid);
+	}
+
+	
+	
 }
