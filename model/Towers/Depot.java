0997a3d6dd470d66d3c7538e91c9908912a4e36b
Benjamin Walters <benscie@email.arizona.edu>
2018-04-23 10:29:12 -0700

    mostly commented towers

--- model/Towers/Depot.java
+++ model/Towers/Depot.java
@@ -1,10 +1,15 @@
 package model.Towers;
 
+
 import java.awt.Point;
 import java.util.Set;
 
 import model.Mobs.Mob;
-
+/**
+ * Depot is the third basic Tower.  It upgrades to a turrent, and ultimately, a Bunker.
+ * 
+ * @author Ben Walters
+ */
 public class Depot extends Tower{
 	 public Depot(Point loc) {
 		 super(0, "Library", loc, Range.DEMO_RANGE, "file:assets/images/tower/depot.png");
--- model/Towers/Tower.java
+++ model/Towers/Tower.java
@@ -1,10 +1,7 @@
 package model.Towers;
 
-
-
 import model.Projectile;
 import model.Mobs.Mob;
-
 
 import java.awt.Point;
 import java.util.HashSet;
@@ -18,54 +15,20 @@ import javafx.scene.image.Image;
 import model.Maps.Metric;
 import model.Mobs.Mob;
 
-// Towers are purchased by the player and placed strategically along the paths.
-
-// Towers will attack enemeies that pass by (in its range of attack). Examples:
-// * Gemcraft uses towers powered by gems and the combination of gems determine
-//   damage, range, etc.
-// * Kingdon Rush uses a medieval setting with archery towers and warrior towers.
-// * Be creative! You can decide how towers should attack enemies. 
-//   It can be single target, or multi-target!
-
-// Each tower has a different damage amount, range of attack, and rate of attack.
-
-// Each tower has a different way of attacking enemies. Possibilities include
-// * attack multiple enemies
-// * teleport enemies
-// * slow enemies
-// * poison enemies
-// * confusing enemies so that they either walk back towards their start or maybe
-//   they spin in a circle for a few seconds
-// * Be Creative!
-
-// A player can view information about a tower before purchasing it and/or
-// by clicking one that has been placed.
-
-// Each tower should cost the player something, could be gold, etc.
-
-// Towers must be upgradeable to have more range, fire rate, or damage.
-
-// Tower upgrades change the look of the tower.
-
-// Towers must have a minimum of 1 tower upgrade.
-
-// A tower's range is visible when the tower is clicked. If a tower is upgraded,
-// this information should also be updated.
-
-// Tower attacks are animated.
-
-// Towers cannot be built such that they block the path of enemies.
-
-// A player can build at least three different towers. These three towers must 
-// be inherently different. You cannot just change the initial damage and range 
-// of a tower, for example, for a tower to qualify as different. A different 
-// tower might involve a different type of attack or a different animation. 
-// (e.g. a lightning tower that attacks 3 enemies at the same time)
-
-// All attacks from the towers must be animated.
+/**
+ * Tower is an abstract class housing the attributes and responsibilities of
+ * individual towers. There are three basic tower types, each of which can be
+ * upgraded into two different towers, for a total of nine unique towers.
+ * 
+ * Towers detect enemies in range and instantiated projectiles to deal damage to
+ * the nearest Mob.
+ * 
+ * @author Ben Walters
+ *
+ */
 
 public abstract class Tower {
-	
+
   protected String name;
   protected int cost;
 
@@ -74,101 +37,122 @@ public abstract class Tower {
   protected Range range;
 
   private String imageFilePath;
-	
-  public Tower(int cost, String name,
-	           Point location, Range range, 
-	           String imageFP
-	           ) {
-    
-	this.cost = cost;
+  
+  /**
+   * 
+   * @param cost - price to the player to instantiate a tower
+   * @param name - name of the tower
+   * @param location - the x,y Point location the tower exists on the map
+   * @param range - the radius around which a tower can detect and fire at mobs.
+   * @param imageFP - the file path to the image representing the tower, images are retrieved by flyweight
+   */
+  public Tower(int cost, String name, Point location, Range range, String imageFP) {
+
+    this.cost = cost;
     this.name = name;
     this.location = location;
     this.range = range;
-	  
     imageFilePath = imageFP;
-	  
     initializeTower();
   }
 
-  
-  /* initializeTower
-   * initializes tower by starting a thread for it to check for nearby mobs
-   * Parameters: None
-   * Returns: None
-  */
+  /**
+   * initializeTower initializes tower by starting a thread for it to check for
+   * nearby mobs Parameters: None Returns: None
+   */
   private void initializeTower() {
-    
+
     towerAnxiety = new Thread(new Runnable() {
-    	
+
       @Override
       public void run() {
-    	try {
-          while(true) {
-          
-            Thread.sleep((long) 60*ControllerMain.UPDATE_FREQUENCY);
-            
+        try {
+          while (true) {
+
+            Thread.sleep((long) 60 * ControllerMain.UPDATE_FREQUENCY);
+
             Set nearbyMobs = getNearbyMobs();
             if (!nearbyMobs.isEmpty()) {
               shoot(nearbyMobs);
             }
-            
-            if(!ControllerMain.isPlaying) {
-            	break;
+
+            if (!ControllerMain.isPlaying) {
+              break;
             }
-          }  
-          
+          }
+
           System.out.println("Tower Thread: Ended");
-    	} catch (InterruptedException e) {
+        } catch (InterruptedException e) {
           e.printStackTrace();
         }
       }
     });
-    
+
     towerAnxiety.start();
   }
 
-  
-
   abstract protected void shoot(Set<Mob> nearbyMobs);
 
-  
+  /**
+   * Tower iterates over all mobs represented in the state of the game, determining which is closes to it at a given
+   * instant
+   * @param nearbyMobs - a list of the nearest mobs on the map to the tower
+   * @return the closest mob to the Tower
+   */
   protected Mob getClosestMob(Set<Mob> nearbyMobs) {
     Iterator<Mob> itr = nearbyMobs.iterator();
-	Mob closest = itr.next();
+    Mob closest = itr.next();
     while (itr.hasNext()) {
       Mob nextMob = itr.next();
       if (isCloser(closest, nextMob)) {
-	    continue;
-	  } else {
-	    closest = nextMob;
+        continue;
+      } else {
+        closest = nextMob;
       }
     }
     return closest;
   }
 
+  /**
+   * Compares two mob's locations and determines which is close to the tower.
+   * 
+   * @param closest the current closest mob to the tower
+   * @param nextMob the mob the closest mob is being compared to
+   * @return
+   */
   private boolean isCloser(Mob closest, Mob nextMob) {
     double winnerDist2 = Metric.distanceSquared(location, closest.getCurrentLocation());
-	double nextDist2 = Metric.distanceSquared(location, nextMob.getCurrentLocation());
-	return winnerDist2 < nextDist2;
+    double nextDist2 = Metric.distanceSquared(location, nextMob.getCurrentLocation());
+    return winnerDist2 < nextDist2;
   }
-	  
-	  
+
+  /**
+   * Creates a set of Mobs which are close to the tower, filtering out those that a sufficiently distant by querying
+   * the mobs collection in the controller main.
+   * @return a HashSet of nearby mobs
+   */
   private Set getNearbyMobs() {
     Set nearbyMobs = new HashSet();
     Iterator<Mob> itr = ControllerMain.mobs.iterator();
-    while(itr.hasNext()) {
-       Mob nextMob = itr.next();
-       if (isNear(nextMob)) {
-         nearbyMobs.add(nextMob);
-       }
+    while (itr.hasNext()) {
+      Mob nextMob = itr.next();
+      if (isNear(nextMob)) {
+        nearbyMobs.add(nextMob);
+      }
     }
     return nearbyMobs;
   }
 
+  /**
+   * Determines if a mob is near to the tower by comparison with it's location relative to the range of the tower
+   * @param nextMob - Mob to be checked for nearness
+   * @return
+   */
   private boolean isNear(Mob nextMob) {
     return Metric.closeEnough(nextMob.getCurrentLocation(), location, range.toDouble());
   }
 
+  /** GETTERS AND SETTERS FOLLOW */
   public String getImageFilePath() {
     return imageFilePath;
   }
@@ -176,24 +160,21 @@ public abstract class Tower {
   public void setImageFilePath(String imageFilePath) {
     this.imageFilePath = imageFilePath;
   }
-  
+
   public Image getImage() {
     return ControllerMain.getGraphic(this.getImageFilePath());
   }
-  
+
   public double getX() {
     return location.getX();
   }
-  
+
   public double getY() {
     return location.getY();
   }
-  
-  public int getCost()
-  {
-	  return cost;
+
+  public int getCost() {
+    return cost;
   }
-  
+
 }
-
-
